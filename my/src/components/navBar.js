3d2d54deb13ffac937705bada51ab53112b35d39
martin ymaz <martin_Ymaz@hotmail.com>
2021-12-15 14:51:02 -0300

    my fitnes

--- my/src/components/navBar.js
+++ my/src/components/navBar.js
@@ -3,11 +3,24 @@ import react from 'react'
 
 function navBar(){
     return ( 
-    <div>
-        <a>
-            inicio 
-        </a>
-    </div>
-    )
+  
+     
+		
+			<div class="menu">
+				<li><a href="#">Inicio</a></li>
+				<li><a href="#">Nosotros</a></li>
+				<li><a href="#">Servicios</a>
+					<ul class="submenu">
+						<li><a href="#">Servicio #1</a></li>
+						<li><a href="#">Servicio #2</a></li>
+						<li><a href="#">Servicio #3</a></li>
+					</ul>
+				</li>
+				<li><a href="#">Contacto</a></li>
+			</div >
+		
+
+  
+    ) 
 }
 export default navBar;
